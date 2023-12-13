42e30f6ce2509dcd8c5c031cdfb9302687648786
johndoekim <amok0316@gmail.com>
2023-12-13 17:32:29 +0900

    ui 및 기능 분리

--- src/app/(route)/mysetting/page.tsx
+++ src/app/(route)/mysetting/page.tsx
@@ -1,67 +1,113 @@
+'use client';
+
 import Image from 'next/image';
+import { useState } from 'react';
 import { MdPeopleAlt } from 'react-icons/md';
+import { MySettingLayout } from '@/components/setting/mySettingLayout';
 
 export default function mysetting() {
+    const [nickEditOn, setNickEditOn] = useState(false);
+    const [velogTitleEditOn, setVelogTitleEditOn] = useState(false);
+    const [emailEditOn, setEmailEditOn] = useState(false);
+    const [acountDeleteOn, setAccountDeleteOn] = useState(false);
+
+    const handleNickEdit = () => {
+        setNickEditOn(true);
+    };
+
+    const handleVelogTitleEdit = () => {
+        setVelogTitleEditOn(true);
+    };
+
+    const handleEmailEdit = () => {
+        setEmailEditOn(true);
+    };
+
+    const handleAccountDelete = () => {
+        setAccountDeleteOn(true);
+    };
+
+    const nickEdit =
+        nickEditOn == false ? (
+            <div>
+                <p className="font-semibold text-2xl mb-2">닉네임</p>
+                <p className="text-base mb-2 text-velogauthgray-300">소개말</p>
+                <button
+                    onClick={handleNickEdit}
+                    className="text-base text-velogauthgreen-100 underline hover:opacity-80"
+                >
+                    수정
+                </button>
+            </div>
+        ) : (
+            <div className="flex flex-col">
+                <input
+                    className="placeholder-black py-1 px-2 font-semibold text-2xl text-black mb-2 border-velogauthgray-400 border-2 rounded"
+                    placeholder="닉네임"
+                />
+                <input
+                    className="placeholder-black py-1 px-2 text-xl text-black mb-2 border-velogauthgray-400 border-2 rounded"
+                    placeholder="소개말"
+                />
+                <button className="rounded mt-2 ml-auto text-base px-4 py-1 text-white bg-velogauthgreen-100 hover:opacity-80">
+                    저장
+                </button>
+            </div>
+        );
+
+    const velogTitleEdit =
+        velogTitleEditOn == false ? (
+            <>
+                <p className="text-base">벨로그 제목내용</p>
+                <button
+                    onClick={handleVelogTitleEdit}
+                    className="ml-auto text-velogauthgreen-100 underline hover:opacity-80"
+                >
+                    수정
+                </button>
+            </>
+        ) : (
+            <>
+                <input className="placeholder-black px-2 text-lg text-black border-velogauthgray-400 border-2 rounded" />
+                <button className="bg-velogauthgreen-100 px-2 ml-auto hover:opacity-80 rounded text-white">저장</button>
+            </>
+        );
+
+    const emailEdit =
+        emailEditOn == false ? (
+            <>
+                <p className="text-base">이메일</p>
+                <button
+                    onClick={handleEmailEdit}
+                    className="ml-auto text-velogauthgreen-100 underline hover:opacity-80"
+                >
+                    변경
+                </button>
+            </>
+        ) : (
+            <>
+                <input className="placeholder-black px-2 text-lg text-black border-velogauthgray-400 border-2 rounded" />
+                <button className="bg-velogauthgreen-100 px-2 ml-auto hover:opacity-80 rounded text-white">저장</button>
+            </>
+        );
+
+    const acountDelete = (
+        <>
+            <p className="font-semibold text-base mr-5">회원 탈퇴</p>
+            <button className="ml-4 px-4 py-0.5 text-base bg-red-400 rounded text-l text-white hover:opacity-80">
+                회원 탈퇴
+            </button>
+        </>
+    );
+
     return (
         <>
-            <div className="flex items-center justify-center bg-white w-full">
-                <div className="flex flex-col items-start justify-center p-10">
-                    <div className="flex flex-col">
-                        <div className="flex justify-center">
-                            <div className="flex-col flex items-center justify-center">
-                                <MdPeopleAlt
-                                    className="rounded-full border-2 border-velogauthgray-400 bg-gray-300 mb-4"
-                                    size={120}
-                                    alt="사람"
-                                />
-                                <button className="bg-velogauthgreen-100 rounded text-base text-white p-1 hover:opacity-80">
-                                    이미지 업로드
-                                </button>
-                                <button className="bg-white text-velogauthgreen-100 text-base p-2 hover:opacity-80">
-                                    이미지 제거
-                                </button>
-                            </div>
-                            <div className="flex flex-row items-start justify-left p-10 ">
-                                <div>
-                                    <p className="font-semibold text-2xl mb-2">닉네임</p>
-                                    <p className="text-base mb-2 text-velogauthgray-300">소개말</p>
-                                    <button className="text-base text-velogauthgreen-100 underline hover:opacity-80">
-                                        수정
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="flex flex-col bg-white mt-10">
-                        <div className="flex flex-row p-2">
-                            <p className="font-semibold text-base mr-5">벨로그 제목</p>
-                            <p className="text-base">벨로그 제목내용</p>
-                            <button className="ml-auto text-velogauthgreen-100 underline hover:opacity-80">수정</button>
-                        </div>
-                        <p className="text-velogauthgray-300 text-sm ml-2 mt-1 border-b-velogauthgrey-100 border-b-2">
-                            개인 페이지의 좌측 상단에 나타는 페이지 제목입니다
-                        </p>
-
-                        <div className="flex flex-row p-2 mt-2">
-                            <p className="font-semibold text-base mr-5">이메일 주소</p>
-                            <p className="text-base">이메일 </p>
-                            <button className="ml-auto text-velogauthgreen-100 underline hover:opacity-80">변경</button>
-                        </div>
-                        <p className="text-velogauthgray-300 text-sm ml-2 mt-1 border-b-velogauthgrey-100 border-b-2">
-                            회원 인증 또는 시스템에서 발송하는 이메일을 수신하는 주소입니다.
-                        </p>
-                        <div className="flex flex-row p-2 mt-2">
-                            <p className="font-semibold text-base mr-5">회원 탈퇴</p>
-                            <button className="ml-4 px-4 py-0.5 text-base bg-red-400 rounded text-l text-white hover:opacity-80">
-                                회원 탈퇴
-                            </button>
-                        </div>
-                        <p className="text-velogauthgray-300 text-sm ml-2 mt-1">
-                            탈퇴 시 작성하신 포스트 및 댓글이 모두 삭제되며 복구되지 않습니다.{' '}
-                        </p>
-                    </div>
-                </div>
-            </div>
+            <MySettingLayout
+                emailEdit={emailEdit}
+                nickEdit={nickEdit}
+                velogTitleEdit={velogTitleEdit}
+                acountDelete={acountDelete}
+            />
         </>
     );
 }
